ef7a6c109e993103aaa265722148345504c82a23
dominic-mordini <djmordini@stu.naperville203.org>
2025-11-10 08:34:04 -0600

    Started color lab

--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -6,6 +6,7 @@
  * @author (Kevin Hayes)
  * @version (10-19-2016)
  */
+import java.util.Random;
 import java.awt.*;// the color class in awt library
 import java.util.*;
 import java.util.List; // resolves problem with java.awt.List and java.util.List
@@ -31,17 +32,28 @@ public class TestPicture17
      //relative path               dir/folder/file
      Picture apic = new Picture("images\\beach.jpg");
      Picture ferris1 = new Picture("images/2000 ferris wheel2.jpg");
-     Picture selfie = new Picture("images/seflie.jpg");
-     Picture ferris3 = new Picture("images/2000 ferris wheel2.jpg");
+     Picture bMark = new Picture("images/blue-mark.jpg");
+     Picture cycle = new Picture("images/blueMotorcycle.jpg");
+     Picture temple1 = new Picture("images/temple.jpg");
+     
+    
 
-     apic.explore();//!!!!!!!! Displays the picture
-     ferris1.explore();
-     selfie.explore();
+     //!!!!!!!! Displays the picture
+     
+    
      
      //makes an array of pixels
      Pixel[] pixels;
+     Pixel[] pixels2;
+     Pixel[] pixels3;
+     Pixel[] pixels4;
+     Pixel[] pixels5;
      //gets pixels from picture and assigns to pixels array
      pixels = ferris1.getPixels();
+     pixels2 = apic.getPixels();
+     pixels3 = bMark.getPixels();
+     pixels4 = cycle.getPixels();
+     pixels5 = temple1.getPixels();
     
      //how many pixels or how large array
     System.out.println("This is a large array"+pixels.length  );
@@ -49,40 +61,73 @@ public class TestPicture17
 
     /**/
         //access each index, arrray note
-    System.out.println(pixels[17]);
-    //access each pixel pic.methods.getPixel
-    Pixel spot = ferris1.getPixel(100,100);
-    Pixel spot50 = ferris1.getPixel(50,50);
-    Color lesteel = new Color(176,196,222);
+    // System.out.println(pixels[17]);
+    // //access each pixel pic.methods.getPixel
+    // Pixel spot = ferris1.getPixel(100,100);
+    // Pixel spot50 = ferris1.getPixel(50,50);
+    // Color lesteel = new Color(176,196,222);
     
     
-    System.out.println(pixels[17].getColor());
-    System.out.println(spot);
+    // System.out.println(pixels[17].getColor());
+    // System.out.println(spot);
     
-    Pixel spot17 = pixels[17];
-    spot17.setRed(200);
-    spot17.setBlue(150);
-    spot17.setGreen(175);
-    ferris1.explore();
+    // Pixel spot17 = pixels[17];
+    // spot17.setRed(200);
+    // spot17.setBlue(150);
+    // spot17.setGreen(175);
+    // ferris1.explore();
     
-    spot.setColor(Color.yellow);
-    ferris1.explore();
+    // spot.setColor(Color.yellow);
+    // ferris1.explore();
 /*
     pixels[17].setColor(Color.blue);
     spot.setColor(new Color(252,252,252));
     pixels[500034].setColor(Color.blue);
 
     ferris1.explore();
-/*
+/**/
    // loop to access indexes of array or collection
 
     //for each loop spot  is a ?
-    for (Pixel spot : pixels)
-    System.out.println( spot );
-
-
-   
- /**/
+    int red;
+    int blue;
+    int green;
+    for (Pixel spot1 : pixels)
+    {
+        blue = spot1.getBlue();
+        red = spot1.getRed();
+        green = spot1.getGreen();
+        blue = (int)(blue * .25);
+        red = (int)(red * .50);
+        green = (int)(green * 1.5);
+        spot1.setBlue(blue);
+        spot1.setRed(red);
+        spot1.setGreen(green);
+    }
+        //System.out.println( spot1 );
+     ferris1.explore();
+    for (Pixel spot2 : pixels4)
+    {
+        red = spot2.getRed();
+        green = spot2.getGreen();
+        blue = spot2.getBlue();
+        red = red;
+        blue = (red + 5);
+        green = (red - 5);
+        spot2.setBlue(blue);
+        spot2.setRed(red);
+        spot2.setGreen(green);
+    }
+     cycle.explore();
+    for (Pixel spot3 : pixels5)
+    {
+        red = spot3.getRed();
+        green = spot3.getGreen();
+        blue = spot3.getBlue();
+        red = (red + 50);
+        green = (green + 50);
+    }
+ 
 
  /**
   * Method to clear red from picture
