8ee0b1a13d14036beff113503cbfe1cd5bc96ce8
dominic-mordini <djmordini@stu.naperville203.org>
2026-05-06 12:02:41 -0500

    added grayscale and upgraded copy to canvas class

--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -19,30 +19,16 @@ public class TestPicture17
      */
   public static void main(String[] args)
   {
-    Picture pic = new Picture("images/jellyFish.jpg");
-    Pixel[] pixels = pic.getPixels();
+    Picture pic = new Picture("images/turtle.jpg");
+    Picture canvas = new Picture("images/posterCanvas.jpg");
     
-   
-        for (Pixel p : pixels) {
-            int gray = (p.getRed() + p.getGreen() + p.getBlue()) / 3;
-            p.setRed(gray);
-            p.setGreen(gray);
-            p.setBlue(gray);
-        }
-
-       
-        int min = 255;
-        int max = 0;
-
-        for (Pixel p : pixels) {
-            int gray = p.getRed();  // red = green = blue = gray
-            if (gray < min) min = gray;
-            if (gray > max) max = gray;
-        }
-
-        mirrorVertical(pic);
-        pic.explore();
+    grayScale(pic);
+    //mirrorVertical(pic);
+    mirrorHoriz(pic);
+    copytoCanvas(pic,canvas,0,0);
+    canvas.explore();
         
+    
      
     
      
@@ -76,21 +62,44 @@ public class TestPicture17
       }
   }//mirrorVertical
   /**
+   * Method to mirror a vertical line in the middle of the picture based
+   * on the width
+   */
+  public static void mirrorHoriz(Picture source)
+  {
+      int height = source.getHeight();
+      int mirrorPoint = height/2;
+      Pixel leftPixel = null;
+      Pixel rightPixel = null;
+      
+      //loop through all the rows
+      for(int y = 0; y < source.getHeight(); y++)
+      {
+          //loop from 0 to the middle
+          for(int x = 0; x < mirrorPoint; x++)
+          {
+              leftPixel = source.getPixel(x,y);
+              rightPixel = source.getPixel(height -1 -x,y);
+              rightPixel.setColor(leftPixel.getColor());
+          }
+      }
+  }//mirrorVertical
+  /**
    * copy one pic to another pic/canvas
    * add two ints to params to place you want pic on the target
    */
-  public static void copytoCanvas(Picture source, Picture target)
+  public static void copytoCanvas(Picture source, Picture target, int x, int y)
   {
       Pixel sourcePix = null;
       Pixel targetPix = null;
       
       //loop through columns (targetX is starting point on Canvas) sourceX+=2 - smaller copy every other pixel
       //                                                           sourceX+=.5 - larger, copy every pixel twice, cast as int in the getPixel & set color
-      for(int sourceX = 0, targetX = 0; sourceX < source.getWidth(); sourceX++, targetX++)
+      for(int sourceX = 0, targetX = x; sourceX < source.getWidth(); sourceX++, targetX++)
       {
           //loop through the rows                                         sourceY+=2 - smaller
           //                                                              sourceY+=.5 - larger
-          for(int sourceY = 0, targetY = 0; sourceY < source.getHeight(); sourceY++, targetY++)
+          for(int sourceY = 0, targetY = y; sourceY < source.getHeight(); sourceY++, targetY++)
           {
               sourcePix = source.getPixel(sourceX, sourceY);
               targetPix = target.getPixel(targetX, targetY);
@@ -98,26 +107,24 @@ public class TestPicture17
           }
       }
   }
-  /**
-  * Method to mirror part of the temple picture around a 
-  * vertical line at the mirror point to fix the right side of the roof. [cite: 221]
-  */
-  public static void mirrorTemple(Picture source) 
+  public static void grayScale(Picture source)
   {
-    int mirrorPoint = 276; 
-    Pixel leftPixel = null;
-    Pixel rightPixel = null;
-    int count = 0;
-    for (int y = 27; y < 97; y++) 
-    {
-        for (int x = 13; x < mirrorPoint; x++) 
-        {
-            leftPixel = source.getPixel(x, y); 
-            rightPixel = source.getPixel(mirrorPoint + (mirrorPoint - x), y); 
-            rightPixel.setColor(leftPixel.getColor());
-            count++;
+      Pixel[] pixels = source.getPixels();
+     for (Pixel p : pixels) {
+            int gray = (p.getRed() + p.getGreen() + p.getBlue()) / 3;
+            p.setRed(gray);
+            p.setGreen(gray);
+            p.setBlue(gray);
+     }
+     int min = 255;
+        int max = 0;
+
+        for (Pixel p : pixels) {
+            int gray = p.getRed();  // red = green = blue = gray
+            if (gray < min) min = gray;
+            if (gray > max) max = gray;
         }
-    }
+
   }
 
   
